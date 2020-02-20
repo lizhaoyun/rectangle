bd68194409d300be90acb14acc805940ad571555
lizhaoyun <2571770878@qq.com>
2020-02-20 21:58:05 +0800

    0.1

--- rectangle/rectangle.js
+++ rectangle/rectangle.js
@@ -16,7 +16,32 @@ $(function(){
         $btnCal.click(function () { 
             var w = Number($width.val()),
                 h=Number($height.val());
-            var p=2*(w+h),
+            console.log(w,h);
+            console.log($width.val().split("."));
+            console.log($height.val().split("."));
+
+            var wplength=$width.val().split(".")[1].length;
+            var hplength=$height.val().split(".")[1].length;
+            var c = Math.abs(wplength-hplength);
+            var m = Math.pow(10,Math.pow(10,c));
+            var cm = Math.pow(10,c);
+            console.log(c,cm,m);
+            if(wplength>hplength){
+                var w1 = Number($width.val().replace("."," "));
+                var h1 = Number($height.val().replace("."," "))*cm;
+                console.log(w1,h1);
+
+            }
+            else{
+                var w1 = Number($width.val().replace("."," "))*cm;
+                var h1 = Number($height.val().replace("."," "));
+                console.log(w1,h1);
+
+            }
+            var sum=(w1+h1)/m;
+            // var p=2*(w+h).toFixed(2),
+            //     a=w*h;
+            var p=2*sum,
                 a=w*h;
             $length.val(p);
             $area.val(a);
